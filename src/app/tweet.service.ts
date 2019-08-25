d27e92feaab1488968e4bc4e48e6676665ae65b8
Yurii O <onyshchak.com@gmail.com>
2019-08-25 20:40:44 +0300

    make it better

--- src/app/tweet.service.ts
+++ src/app/tweet.service.ts
@@ -9,10 +9,11 @@ export class TweetService {
 
   constructor(private http: HttpClient) {}
 
-  private tweetsUrl = 'http://localhost:3000/';
-  private tweetsUrlHeroku = 'https://twitter-timeline-256.herokuapp.com/';
+  private tweetsUrl = 'http://localhost:3000/tweets/';
+  private tweetsUrlHeroku = 'https://twitter-timeline-256.herokuapp.com/tweets/';
 
   getTweets(name): Observable<Tweet[]> {
+    console.log(this.tweetsUrl + name);
     return this.http.get<Tweet[]>(this.tweetsUrl + name);
   }
 }
