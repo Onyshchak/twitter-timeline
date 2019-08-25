65d8c142303f925569af8a1dab08b0b9399168b8
Yurii O <onyshchak.com@gmail.com>
2019-08-25 20:13:54 +0300

    make it better

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const Twitter = require('twitter');
 
 const app = express();
 
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static('./dist/twitter-timeline'));
 
@@ -20,7 +20,7 @@ const client = new Twitter({
 
 app.get('/tweets/:name', async (req, res) => {
 
-  const params = {screen_name: req.params.name, count: 30, tweet_mode:'extended'};
+  const params = {screen_name: req.params.name, count: 32, tweet_mode:'extended'};
   const modifTweets = [];
 
   try {
@@ -39,7 +39,7 @@ app.get('/tweets/:name', async (req, res) => {
           nickname: tweet.user.screen_name,
           img: tweet.user.profile_image_url,
           timePost: tweet.created_at.split(' ').slice(1, 3).join(' '),
-          tweetText: tweet.full_text.substring(0, tweet.full_text.lastIndexOf(' ')),
+          tweetText: tweet.full_text,
           tweetImg: image,
           actionsCount: [tweet.retweet_count, tweet.favorite_count]
         };
@@ -53,7 +53,7 @@ app.get('/tweets/:name', async (req, res) => {
 });
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/twitter-timeline/index.html'));
+  res.sendFile(path.join(__dirname, './dist/twitter-timeline/index.html'));
 });
 
 app.listen(process.env.PORT || 3000, () => {
